5ad88942570205def7ea107fea3a5939c6143198
oyugirachel <oyugirachel20@gmail.com>
2021-02-01 16:02:00 +0300

    adding AI to black jack

--- main.go
+++ main.go
@@ -22,6 +22,38 @@ func(h Hand) DealerString() string{
 	return h[0].String() + ",**HIDDEN**"
 
 }
+// Score Function
+func (h Hand) Score() int{
+	minScore := h.MinScore()
+	if minScore >11{
+		return minScore
+	}
+	for _, c:=range h{
+		if c.Rank ==deck.Ace{
+			// ace is currently worth 1, and we are changing it to be worth 11 
+			//11-1 =10
+			return minScore + 10
+		}
+		
+	}
+	return minScore
+
+}
+// MinScore function
+func (h Hand) MinScore() int{
+	score := 0
+	for _, c:= range h{
+		score += min(int(c.Rank),10)
+	}
+	return score 
+
+}
+func min(a,b int) int{
+	if a<b{
+		return a
+	}
+	return b
+}
 func main(){
 	cards := deck.New(deck.Deck(3),deck.Shuffle)
 	var card deck.Card
@@ -37,8 +69,8 @@ func main(){
 	var input string
 	// s representing standing
 	for input != "s"{
-		fmt.Println("Player:",player)
-		fmt.Println("Dealer:",dealer.DealerString())
+		fmt.Println("Player:",player,"\nScore",player.MinScore())
+		fmt.Println("Dealer:",dealer, "\nScore",dealer.MinScore())
 		fmt.Println("What will you do? (h)it,(s)tand")
 		// Reading the users input
 		fmt.Scanf("%s\n",&input)
@@ -51,11 +83,31 @@ func main(){
 	
 
 	}
+	// if dealer score <= 16 we hit
+	// if dealer has a soft 17, then we hit
+	for dealer.Score() <=16 || (dealer.Score() ==17 && dealer.MinScore() !=17){
+		card, cards = draw(cards)
+		dealer = append(dealer,card)
+     
+	}
+	pScore, dScore := player.Score(), dealer.Score()
 	// Printing out the final scores
 	fmt.Println("**FINAL HANDS**")
-	fmt.Println("Player",player)
-	fmt.Println("Dealer",dealer)
-	
+	fmt.Println("Player",pScore)
+	fmt.Println("Dealer",dScore)
+	switch{
+	case pScore>21:
+		fmt.Println("You busted")
+	case dScore>21:
+		fmt.Println("Dealer busted")
+	case pScore>dScore:
+		fmt.Println("You win!")
+	case dScore >pScore:
+		fmt.Println("You lose")
+	case dScore==pScore:
+		fmt.Println("Draw")
+
+	}
 
 
    
